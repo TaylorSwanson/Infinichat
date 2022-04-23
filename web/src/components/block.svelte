f1dee2faf76c289b18b8c19a90add9fab2462a91
Taylor Swanson <contact@taylorswanson.com>
2022-04-23 09:55:26 -0600

    Clear svelte up

---
+++ web/src/components/block.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// export let name: string;
+</script>
+
+<main>
+	<div class="block">
+    <p>content</p>
+  </div>
+</main>
+
+<style>
+	.block {
+    width: 100px;
+    height: 100px;
+    background-color: rgb(120, 120, 120);
+  }
+</style>
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 		mainFields: ["svelte", "browser", "module", "main"]
 	},
 	output: {
-		path: path.join(__dirname, "/public"),
+		path: path.join(__dirname, "/dist"),
 		filename: "[name]-[contenthash:8].js",
 		chunkFilename: "[name].[id].js",
 		clean: true
